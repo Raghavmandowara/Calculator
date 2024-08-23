67923672676233a5fe92c17f6d3fcd6cfc5f8703
Raghavmandowara <raghavmandowara2005@gmail.com>
2024-08-23 20:53:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+// Add functions for other operations (e.g., square root, percentage)
